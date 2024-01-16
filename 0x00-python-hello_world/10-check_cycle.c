e93f85e3cbfe71ceffb56647fa57a5ecbbffc7b9
“Ahmad <ahmadabdelrazik159@gmail.com>
2024-01-16 08:57:05 +0200

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,16 +1,10 @@
-/**
-* @author       : Ahmad Abdelrazik (ahmadabdelrazik159@gmail.com)
-* @created      : Tuesday Jan 16, 2024 08:47:31 EET
-* @filename     : 10-check_cycles.c
-*/
-
 #include "lists.h"
 
 /**
  * check_cycle - check if there is a cycle in the linked list
  * @list: the linked list head
  *
- * @Return: 0 if there is no cycle, 1 if there is a cycle
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
@@ -18,7 +12,7 @@ int check_cycle(listint_t *list)
     listint_t *slow = list;
 
     if (list == NULL)
-        return 0;
+        return (1);
 
     while (fast != NULL && slow != NULL)
     {
@@ -29,10 +23,9 @@ int check_cycle(listint_t *list)
 
         slow = slow->next;
 
-        if (fast == slow)
-            return 1;
+        if (fast == slow && fast != NULL)
+            return (1);
     }
-
 
     return (0);
 }
