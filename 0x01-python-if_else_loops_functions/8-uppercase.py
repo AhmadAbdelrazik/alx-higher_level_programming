a1089cea6a6ed86e64293fcfa22f2d80ebc313e4
“Ahmad <ahmadabdelrazik159@gmail.com>
2024-01-18 06:30:19 +0200

    the .format thing

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     str = str + '\n'
     for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            print(chr(ord(i) - ord('a') + ord('A')), end="")
+            print("{}".format(chr(ord(i) - ord('a') + ord('A'))), end="")
         else:
-            print(i, end="")
+            print("{}".format(i), end="")
