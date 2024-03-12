d3ec9da094b51c38d5fcefffa8cb1a27ff4d592c
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-03-12 17:36:19 +0200

    Fresh Start

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-for i in range(26):
-    if chr(ord('a') + i) == 'e' or chr(ord('a') + i) == 'q':
-        continue
-    print("{}".format(chr(ord('a') + i)), end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    str = str + '\n'
-    for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            print("{}".format(chr(ord(i) - ord('a') + ord('A'))), end="")
-        else:
-            print("{}".format(i), end="")
