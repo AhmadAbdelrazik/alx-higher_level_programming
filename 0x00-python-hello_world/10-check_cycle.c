54fba8379424a3228a9036e9e378b1f05beafbf2
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-03-12 20:30:33 +0200

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,24 +1,30 @@
 #include "lists.h"
 
-int check_cycle(listint_t *list) {
-	if (list == NULL) {
-		return 0;
-	}
+/**
+ * check_cycle - check if the list contains a cycle
+ * @list: the list.
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	if (list == NULL)
+		return (0);
 
 	listint_t *sp = list;
 	listint_t *fp = list;
 
-	while (fp != NULL && sp != NULL) {
+	while (fp != NULL && sp != NULL)
+	{
 		sp = sp->next;
 		fp = fp->next;
 
 		if (fp != NULL)
 			fp = fp->next;
 
-		if (sp == fp) {
-			return 1;
-		}
+		if (sp == fp)
+			return (1);
 	}
 
-	return 0;
+	return (0);
 }
