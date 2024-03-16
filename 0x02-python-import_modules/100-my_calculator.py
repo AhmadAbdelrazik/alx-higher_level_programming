e72dee84c5d65e4b8e6fb1d357963a2004927af4
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-03-17 00:50:15 +0200

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,23 +8,22 @@ if __name__ != "__main__":
 argc = len(argv)
 
 if argc != 4:
-	print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-	exit(1)
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
 
 a = int(argv[1])
 operator = argv[2]
 b = int(argv[3])
+func = None
 
 if operator == "+":
-	print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
 elif operator == "-":
-	print("{} - {} = {}".format(a, b, sub(a, b)))
-elif operator == "-":
-	print("{} / {} = {}".format(a, b, div(a, b)))
-elif operator == "-":
-	print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+elif operator == "/":
+    print("{} / {} = {}".format(a, b, div(a, b)))
+elif operator == "*":
+    print("{} * {} = {}".format(a, b, mul(a, b)))
 else:
-	print("Unknown operator. Available operators: +, -, * and /")
-	exit(1)
-
-
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
