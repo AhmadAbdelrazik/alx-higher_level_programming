01760694cdce1e11529a2ef72b209f12f3eec175
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-03-17 01:29:22 +0200

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_str = ""
     for i in my_string:
-        if i != "c" or i != "C":
+        if i != "c" and i != "C":
             new_str += i
 
-    return i
+    return new_str
