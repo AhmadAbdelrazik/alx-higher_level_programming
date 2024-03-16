32882f9911f710a6aa9fb6bdbee9da72b399fa24
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-03-16 21:04:28 +0200

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
-ls = dir(hidden_4)
-print(ls)
-for i in ls:
-    if i[:2] != '__':
-        print(i)
+
+if __name__ == "__main__":
+    ls = dir(hidden_4)
+    print(ls)
+    for i in ls:
+        if i[:2] != '__':
+            print(i)
