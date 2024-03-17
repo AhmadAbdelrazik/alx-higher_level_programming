5c6cda28701bbd482bcba53597d43d41942a211e
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-03-17 20:17:07 +0200

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,38 +1,54 @@
 #include "lists.h"
+#include <stdlib.h>
 
 
 /**
-* is_palindrome - check if the linked list is a palindrome
-* @head: the linked list head.
-*
-* Return: return 1 if palindrome, else return 0
-*/
+ * is_palindrome - check if the linked list is a palindrome
+ * @head: the linked list head.
+ *
+ * Return: return 1 if palindrome, else return 0
+ */
 int is_palindrome(listint_t **head)
 {
-int arr[1000];
-int l = 0, r = 0;
-listint_t *cur;
+	int length = 0;
+	listint_t *cur;
+	int *arr;
+	int l = 0;
+	int r = 0;
 
-if (head == NULL || *head == NULL)
-return (0);
+	if (head == NULL || *head == NULL)
+		return (1);
 
-cur = *head;
+	cur = *head;
 
-while (cur != NULL)
-{
-arr[r] = cur->n;
-r++;
-cur = cur->next;
-}
-r--;
+	while (cur != NULL)
+	{
+		length++;
+		cur = cur->next;
+	}
 
-while (l < r)
-{
-if (arr[l] != arr[r])
-return (0);
-l++;
-r--;
-}
+	arr = malloc(sizeof(int) * length);
 
-return (1);
+	if (arr == NULL)
+		return (1);
+
+	cur = *head;
+
+	while (cur != NULL)
+	{
+		arr[r] = cur->n;
+		r++;
+		cur = cur->next;
+	}
+	r--;
+
+	while (l < r)
+	{
+		if (arr[l] != arr[r])
+			return (0);
+		l++;
+		r--;
+	}
+	
+	return (1);
 }
